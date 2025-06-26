268f2fb2c2db8e2f9285d3dc4837ec654e8dae4f
sidhantkumar <sd247182@dal.ca>
2025-06-25 21:04:50 -0300

    feat: created login activity class

--- app/src/main/java/com/example/gitofy/view/LoginActivity.java
+++ app/src/main/java/com/example/gitofy/view/LoginActivity.java
@@ -1,8 +1,6 @@
 package com.example.gitofy.view;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,29 +10,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.gitofy.R;
 
-public class MainActivity extends AppCompatActivity {
-
-    EditText email, PAT;
-    Button login;
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        initialize();
-    }
-
-
-    public void initialize(){
-        email = findViewById(R.id.emailID);
-        PAT = findViewById(R.id.pat);
-        login = findViewById(R.id.loginButton);
     }
 }
---
+++ app/src/main/java/com/example/gitofy/view/PlatformSelectorActivity.java
@@ -0,0 +1,40 @@
+package com.example.gitofy.view;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.gitofy.R;
+
+public class PlatformSelectorActivity extends AppCompatActivity {
+
+    EditText email, PAT;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        initialize();
+    }
+
+
+    public void initialize(){
+        email = findViewById(R.id.emailID);
+        PAT = findViewById(R.id.pat);
+        login = findViewById(R.id.loginButton);
+    }
+}
