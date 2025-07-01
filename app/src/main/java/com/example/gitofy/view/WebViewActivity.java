2160819fac57af5cb3a4974dafb29eeeeb742b1e
Raj-2204 <rajveerbakhshi@gmail.com>
2025-06-30 23:41:52 -0300

    U2: athourization handling added for github

--- app/src/main/java/com/example/gitofy/view/WebViewActivity.java
+++ app/src/main/java/com/example/gitofy/view/WebViewActivity.java
@@ -1,6 +1,8 @@
 package com.example.gitofy.view;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
@@ -32,17 +34,34 @@ public class WebViewActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                view.loadUrl(request.getUrl().toString());
-                return true;
-            }
+                Uri url = request.getUrl();
 
-            @Override
-            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-                Toast.makeText(getApplicationContext(), "Error: " + error.getDescription(), Toast.LENGTH_SHORT).show();
+                // Check if GitHub is redirecting to your app
+                if (url.toString().startsWith("myapp://callback")) {
+                    String code = url.getQueryParameter("code");
+
+                    if (code != null) {
+                        Toast.makeText(getApplicationContext(), "Code: " + code, Toast.LENGTH_LONG).show();
+                        Log.d("OAuth", "Code received: " + code);
+
+                        // TODO: Exchange code for access_token here or pass it to another activity
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Authorization failed", Toast.LENGTH_SHORT).show();
+                    }
+
+                    return true; // Prevent WebView from loading this redirect
+                }
+
+                // Otherwise continue loading
+                return false;
             }
         });
 
-        String authUrl = "https://github.com/login/oauth/authorize?client_id=Ov23lish7hRvDchbveEI&scope=read:user";
+        String authUrl = "https://github.com/login/oauth/authorize" +
+                "?client_id=Ov23lish7hRvDchbveEI" +
+                "&redirect_uri=myapp://callback" +
+                "&scope=repo";
+
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
         webView.clearCache(true);
